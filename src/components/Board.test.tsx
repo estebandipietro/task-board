163a3ebbebb5c093b91e7d641a0a373c8ef01c07
Esteban Di Pietro <chapa96_12@hotmail.com>
2024-05-17 20:34:47 +0200

    add reset button test

--- src/components/Board.test.tsx
+++ src/components/Board.test.tsx
@@ -1,42 +1,39 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { describe, test, expect, vi } from 'vitest';
 import Board from './Board';
-
-vi.mock('../logic/cardMovement', () => ({
-    moveCard: vi.fn(),
-}));
-
-vi.mock('../utils/localStorage', () => ({
-    getBoardFromLocalStorage: vi.fn(),
-    saveBoardToLocalStorage: vi.fn(),
-}));
-
-const testData = {
-    backlog: [{ id: '1', content: 'Task 1' }],
-    todo: [{ id: '2', content: 'Task 2' }],
-    doing: [{ id: '3', content: 'Task 3' }],
-    done: [{ id: '4', content: 'Task 4' }],
-};
+import * as localStorageUtils from '../utils/localStorage'
 
 describe('Board', () => {
-    test('renders columns based on data', () => {
-        vi.mock('../utils/localStorage', () => ({
-            getBoardFromLocalStorage: () => testData,
-        }));
+    test('renders without errors', () => {
+        render(<Board />);
+    });
 
+    test('renders reset button', () => {
         const { getByText } = render(<Board />);
+        const resetButton = getByText('Reset Board');
+        expect(resetButton).toBeInTheDocument();
+    });
 
-        // Check if column titles are rendered
-        expect(getByText('BACKLOG')).toBeInTheDocument();
-        expect(getByText('TODO')).toBeInTheDocument();
-        expect(getByText('DOING')).toBeInTheDocument();
-        expect(getByText('DONE')).toBeInTheDocument();
-
-        // Check if tasks are rendered in each column
-        Object.values(testData).forEach(tasks => {
-            tasks.forEach(task => {
-                expect(getByText(task.content)).toBeInTheDocument();
-            });
+    test('resets board and local storage on button click', () => {
+        const localStorageSpy = vi.spyOn(localStorageUtils, 'saveBoardToLocalStorage')
+        const { getByText } = render(<Board />);
+        const resetButton = getByText('Reset Board');
+        fireEvent.click(resetButton);
+        expect(localStorageSpy).toHaveBeenCalledWith({
+            backlog: [
+                { id: '1', content: 'Task 1' },
+                { id: '2', content: 'Task 2' },
+            ],
+            todo: [
+                { id: '3', content: 'Task 3' },
+                { id: '4', content: 'Task 4' },
+            ],
+            doing: [
+                { id: '5', content: 'Task 5' },
+            ],
+            done: [
+                { id: '6', content: 'Task 6' },
+            ],
         });
     });
 });
